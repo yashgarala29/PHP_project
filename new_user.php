dd1c5c623eb601b400b48ececad10218a5ac83ea
Lenovo <Lenovo@YASH>
2020-02-23 18:08:53 +0530

    initial commit

---
+++ new_user.php
@@ -0,0 +1,10 @@
+<head>
+        <title>PHP project</title>
+        <link rel="stylesheet" type="text/css" href="style.css"> 
+ </head>
+<html>
+    
+    <label>name</label>
+    <input type="text" name="name" value="" />   
+    
+</html>
