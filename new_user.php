876f837f59bd858957b42082ef6250c5374e97c7
Lenovo <Lenovo@YASH>
2020-02-26 20:21:25 +0530

    initial commit

--- new_user.php
+++ new_user.php
@@ -1,10 +1,42 @@
+<html>
 <head>
         <title>PHP project</title>
-        <link rel="stylesheet" type="text/css" href="style.css"> 
+         <link rel="stylesheet" type="text/css" href="style.css"> 
  </head>
-<html>
+
+    <center>
+        <div class="ali">
+        <form action="firstpage.html" method="POST">
+       
+            <table border="0">
+
+                <tbody>
+                    <tr>
+                        <td>name</td>
+                        <td> <input type="text" name="name" value="" required="" />   </td>
+                    </tr>
+                    <tr>
+                        <td>password</td>
+                        <td> <input type="password" name="pass" value="" required=""> </td>
+                    </tr>
+                    <tr>
+                        <td>re enter password</td>
+                        <td><input type="password" name="rpass" value="" required=""></td>
+                    </tr>
+                    <tr>
+                        <td>email</td>
+                        <td> <input type="email" name="email" value="" required=""></td>
+                    </tr>
+                    <tr >
+                        <td  colspan="2"> <button class="dropbtn">Dropdown</button> </td>
+                       
+                    </tr>
+                </tbody>
+            </table>
+
+         </form>
+      </div>
+        </center>
     
-    <label>name</label>
-    <input type="text" name="name" value="" />   
     
 </html>
