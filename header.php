31c4039f3ef23c06ca74b01944dcd90bec7f7ea4
Lenovo <Lenovo@YASH>
2020-03-15 19:58:12 +0530

    logout added

--- header.php
+++ header.php
@@ -10,23 +10,26 @@
 <body>
 
 <div class="navbar1">
+    <img src="photo/log.png" width="50" height="50" alt="ydn blogs" style="float: left;"/>
+
   <a href="#home">Home</a>
-  <a href="">News</a>
+  <!--<a href="">News</a>-->
      <?php session_start();
                     if(isset($_SESSION['username']))
                     {
                           echo "<a href='question.php'>ask question</a>";
+                          echo '<div>WEL-COME<a href="#home" >'.$_SESSION["username"].'</div></a>';
                     }
       ?>
   <div class="dropdown1">
-      
-    <button class="dropbtn1">Dropdown 
-    </button>
-    <div class="dropdown-content1">
-      <?php
-     
-                    if(!isset($_SESSION['username']))
-                          {
+      <?php 
+      if(!isset($_SESSION['username'])){
+    echo '<button class="dropbtn1">Login here  </button>
+    <div class="dropdown-content1">';
+//      <?php
+//     
+//                    if(!isset($_SESSION['username']))
+//                          {
                             
                             //$_
                             echo "<form action='check.php'>
@@ -42,8 +45,8 @@
 
                               </tr>
                               <tr>
-                                  <td><button class='dropbtn1' style='background-color: #4CAF50;'>Dropdown</button></td>
-                                  <td><a href='new_user.php'>new user</td>
+                                  <td><button class='dropbtn1' style='background-color: #4CAF50;'>Login</button></td>
+                                  <td><a href='new_user.php' style='color: black;'>new user</td>
                               </tr>
                           </tbody>
                       </table>
@@ -51,8 +54,13 @@
                       </form>";
                             }
                             else{
-                                echo "welcome  ".$_SESSION['username']."<br><br>";
+                               
+                                
+  
                                 echo "<form action='logout.php'><button class='dropbtn1' style='background-color: #4CAF50; width: 100%;'>logout</button></form>";
+//                                echo '<form action="logout.php"><button class="dropbtn1">Logout</button>';
+//                                echo "welcome  ".$_SESSION['username']."<br><br>";
+//                                echo "<form action='logout.php'><button class='dropbtn1' style='background-color: #4CAF50; width: 100%;'>logout</button></form>";
                             }
                      ?>
     </div>
